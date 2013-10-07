9474d283fc822f9e003c99e7b3b8487c1e8c5648
John Chapin <john.chapin@gmail.com>
2013-10-07 12:34:04 +0800

    Move DefaultHttpClient to client.http ns

--- src/slouch/client/http.clj
+++ src/slouch/client/http.clj
@@ -1,6 +1,22 @@
 (ns slouch.client.http
-   (:refer-clojure :exclude [send]))
+  (:refer-clojure :exclude [send])
+  (:require [http.async.client]))
 
 (defprotocol HttpClient
   (send [this url body])
   (close [this]))
+
+(deftype DefaultHttpClient [client]
+  slouch.client.http/HttpClient
+  (send [this url body]
+    (let [response (-> (http.async.client/POST client url :body body)
+                       http.async.client/await
+                       (update-in ,,, [:status] deref)
+                       (update-in ,,, [:body] deref))]
+      [(get-in response [:status :code])
+       (get-in response [:body])]))
+  (close [this]
+    (http.async.client/close client)))
+
+(defn new-client []
+  (DefaultHttpClient. (http.async.client/create-client)))
